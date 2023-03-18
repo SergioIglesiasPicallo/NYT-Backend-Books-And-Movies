ebf78e1f1299b1fa380e0d84c709332d0b45c385
Sergio Iglesias <SergioIglesias@MacBook-Pro-de-Adrian.local>
2023-03-18 10:31:44 +0100

    Feat: Set up 2 + provisional middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,29 @@
+const jsonwebtoken = require('jsonwebtoken');
+const db = require('../models/index.js');
+const User = db.User;
+
+const ensureAuthenticated = async (req, res, next) => {
+    if (req.path.includes('/auth')) return next();
+
+    !req.headers.authorization &&
+        res.status(403).json({ message: 'You are not authenticated' });
+
+    const token = req.headers.authorization.split(' ')[1];
+
+    !token && res.status(403).json({ message: 'Invalid token' });
+
+    const payload = jsonwebtoken.decode(token, process.env.TOKEN_SECRET);
+
+    !payload ||
+        (!payload.email && res.status(403).json({ message: 'Invalid token' }));
+
+    const user = await User.findOne({ where: { email: payload.email } });
+
+    !user && res.status(403).json({ message: 'Invalid token' });
+
+    req.user = user;
+
+    return next();
+};
+
+module.exports = ensureAuthenticated;
