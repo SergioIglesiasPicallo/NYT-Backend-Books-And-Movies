f8a95847629ca9771d77474f612ddcd2d8a3f77a
Sergio Iglesias <SergioIglesias@pc-139-125.customer.ask4.lan>
2023-03-20 18:06:51 +0100

    feat:first user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,56 @@
-'use strict'
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class User extends Model {
+        static associate(models) {
+            User.belongsToMany(models.Post, {
+                through: 'postUsers',
+                as: 'UserPosts',
+                foreignKey: 'user_FK',
+            });
+            User.belongsToMany(models.Post, {
+                through: 'postUserFavorites',
+                as: 'postsFavorites',
+                foreignKey: 'user_favorites_FK',
+            });
+        }
+    }
+    User.init(
+        {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+            },
+            email: {
+                type: DataTypes.STRING,
+                unique: true,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            name: {
+                type: DataTypes.STRING,
+            },
+            salt: {
+                type: DataTypes.STRING,
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'User',
+        }
+    );
+    return User;
+};
