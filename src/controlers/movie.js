f39f21eb8ce9c119d69acca871485212636a0404
Sergio Iglesias <SergioIglesias@MacBook-Pro-de-Adrian.local>
2023-03-21 10:34:41 +0100

    feat: first movie controler

---
+++ src/controlers/movie.js
@@ -0,0 +1,53 @@
+const db = require("../models/index.js");
+const Movie = db.movie
+
+const getMovieList = async () => {
+    try {
+        const movieList = await Movie.findAll();
+        return movieList
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getMovieId = async (id) => {
+
+    const movieId = await Movie.findByPk(id)
+    return movieId
+}
+
+
+const createMovie = async ({ title, explanation, url, date }) => {
+
+    try {
+
+        const exists = await Movie.findOne({ where: { title: title } })
+        if (!exists) {
+            const createMovie = await Movie.create({ title, explanation, url, date })
+            return createMovie
+        }
+
+    } catch (error) {
+        console.log("DOCUMENTO YA ESTA CREADO")
+
+    }
+
+}
+
+const updateMovie = async (id, data) => {
+
+    const movieUpdate = await Movie.update(data, {
+        where: { id }
+    });
+    return movieUpdate
+}
+
+const deleteMovie = async (id) => {
+    await Movie.destroy({
+        where: { id }
+    })
+    return true
+}
+
+module.exports = { getMovieList, getMovieId, createMovie, updateMovie, deleteMovie }
